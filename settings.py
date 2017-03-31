78a718ce3287e61a7fbe0d96fa767457ae086a33
daniel <daniel@arilot.com>
2017-03-31 21:36:03 +0300

    fix UPDATE_INTERVAL type

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ SLACK_TOKEN = os.environ.get('SLACK_TOKEN', '')
 SLACK_CHANNEL = os.environ.get('SLACK_CHANNEL', '#alerts')
 
 # Monitoring
-UPDATE_INTERVAL = os.environ.get('UPDATE_INTERVAL', 60)
+UPDATE_INTERVAL = int(os.environ.get('UPDATE_INTERVAL', 60))
 
 ALERTS_LEVELS_EXCLUDE_RAW = os.environ.get('ALERTS_LEVELS_EXCLUDE', 'Normal')
 ALERTS_LEVELS_EXCLUDE = [level.strip() for level in ALERTS_LEVELS_EXCLUDE_RAW.split(',')]

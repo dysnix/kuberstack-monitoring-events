4ec3c38799c6ff9f745d9809ffb5cf891a517348
daniel <daniel@yavorovych.com>
2017-12-26 05:51:03 +0200

    update telegram alerts format

--- alerts/telegram.py
+++ alerts/telegram.py
@@ -6,10 +6,10 @@ bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 def send_alert(host, event_type, event_text, event_time, event_uri, involved_object):
     text = "*{event_type}*: `{cluster}`\n" \
-           "{involved_object_kind}: ```{involved_object_name}```\n" \
-           "Namespace: ```{namespace}```\n" \
-           "Host: ```{host}```\n" \
-           "`{event_text}`".format(involved_object_kind=involved_object.kind,
+           "`{involved_object_kind}: {involved_object_name}\n" \
+           "Namespace: {namespace}\n" \
+           "Host: {host}`\n" \
+           "```{event_text}```".format(involved_object_kind=involved_object.kind,
                                    involved_object_name=involved_object.name,
                                    namespace=involved_object.namespace, cluster=CLUSTER_NAME, host=host,
                                    event_type=event_type, event_text=event_text)

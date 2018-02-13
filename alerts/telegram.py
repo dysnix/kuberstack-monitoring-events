a23de8aaf407521171a57008b35da1097218dc58
daniel <daniel@yavorovych.com>
2018-02-13 19:00:02 +0200

    fix telegram exception if not used

--- alerts/telegram.py
+++ alerts/telegram.py
@@ -1,10 +1,9 @@
 import telegram
 from settings import CLUSTER_NAME, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 
-bot = telegram.Bot(token=TELEGRAM_TOKEN)
-
 
 def send_alert(host, event_type, event_text, event_time, event_uri, involved_object):
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
     text = "*{event_type}: {cluster}*\n" \
            "{involved_object_kind}: {involved_object_name}\n" \
            "Namespace: {namespace}\n" \
